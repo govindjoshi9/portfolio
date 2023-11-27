b15adb2a3064418e737eb86a9edf7977682e4df5
govind <joshigovind740@gmail.com>
2023-11-27 23:22:50 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.scss"
+import Nabar from "./components/Navbar/Nabar";
+const App = () => {
+  return (
+    <div>
+      <section>
+        <Nabar/>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallas</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Navbar/Nabar.jsx
@@ -0,0 +1,27 @@
+import "./navbar.scss"
+
+const Nabar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <span>Govind Prasad</span>
+        <div className="social">
+          <a href="#">
+            <img src="" alt="Facebook" />
+          </a>
+          <a href="#">
+            <img src="" alt="Instagram" />
+          </a>
+          <a href="https://www.linkedin.com/in/govind-prasad-439051207/">
+            <img src="" alt="LinkedInd" />
+          </a>
+          <a href="https://github.com/govindjoshi9">
+            <img src="" alt="github " />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nabar
