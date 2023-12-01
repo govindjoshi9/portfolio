ff80237dd8fce152785d2130330f413f63453d48
govind <joshigovind740@gmail.com>
2023-12-01 21:02:45 +0530

    hero1

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import "./hero.scss"
 const Hero = () => {
   return (
     <div className='hero'>
+      <div className="wrapper">
+
+      <div className="textContainer">
+        <h2>Govind Prasad</h2>
+        <h1>Software developer</h1>
+        <div className="buttons">
+          <button>Resume</button>
+          <button>
+            Contact as
+          </button>
+        </div>
+        <img src="/scroll.png" alt="" />
+      </div>
+      </div>
       <div className="imageContainer">
         <img src="/coder1.jpg" alt="" />
       </div>
