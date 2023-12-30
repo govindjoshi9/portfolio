4d7a9569a5ea58e7511e9fe6e416d67b57e13dd6
govind <joshigovind740@gmail.com>
2023-12-30 23:59:32 +0530

    add

--- src/components/Portfolio/Portflio.jsx
+++ src/components/Portfolio/Portflio.jsx
@@ -1,76 +1,79 @@
-import React, { useRef, useTransition } from 'react'
-import "./portfolio.scss"
-import {motion, useScroll,useSpring} from 'framer-motion'
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const item = [
-    {
-        id: 1,
-        title: "React Commerce",
-        img: "img",
-        dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
-    },
-    {
-        id: 2,
-        title: "Airbnb clone",
-        img: "img",
-        dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
-    },
-    {
-        id: 3,
-        title: "Nextflix clone",
-        img: "img",
-        dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
-    },
-]
+  {
+    id: 1,
+    title: "React Commerce",
+    img: "./img.jpg",
+    dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
+  },
+  {
+    id: 2,
+    title: "Blog App",
+    img: "./img.jpg",
+    dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
+  },
+  
+  {
+    id: 3,
+    title: "YtClone App",
+    img: "./img3.jpg",
+    dec: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ",
+  },
+];
 
- 
 const Single = ({ item }) => {
-    const ref = useRef()
-     const { scrollProgress } = useScroll({
-       target: ref,
-     });
-    const y = useTransition(scrollProgress,[0,1],[-300,300])
-    return (
-      <section ref={ref}>
-        <div className="container">
-          <div className="wrapper">
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
             <img src={item.img} alt="" />
-            <motion.div className="textContainer" style={{ y }}>
-              <h2>{item.title}</h2>
-              <p>{item.dec}</p>
-              <button>See Demo</button>
-            </motion.div>
           </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.dec}</p>
+            <button>See Demo</button>
+          </motion.div>
         </div>
-      </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
+
 const Portflio = () => {
-    const ref = useRef()
+  const ref = useRef();
 
-    const { scrollProgress } = useScroll({
-        target: ref,
-        offset: ["end end", "start start"],
-    })
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
 
-    const scaleX = useSpring(scrollProgress, {
-        stiffness: 100,
-        damping: 30,
-    });
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
 
   return (
-      <div className='portfilio' ref={ref}>
-          <div className="progress">
-              <h1>
-                  Featured Work
-                  <motion.div style={{scaleX:scaleX}} className="progressBar"></motion.div>
-              </h1>
-          </div>
-          {item.map((item) => (
-              <Single item={item} key={item.id} />
-          ))}
+    <div className="portfilio" ref={ref}>
+      <div className="progress">
+        <h1>
+          Featured Work
+          <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        </h1>
+      </div>
+      {item.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Portflio
+export default Portflio;
